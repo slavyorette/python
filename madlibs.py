b01faee87c42c386c6314e359991cc0d9caee444
slavyorette <70397534+slavyorette@users.noreply.github.com>
2020-09-11 15:31:11 -0400

    Update madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -8,10 +8,15 @@ noun1 = input("Enter a another noun: ")
 noun2 = input("Enter a another noun: ")
 noun3 = input("Enter a another noun: ")
 pluralnoun = input("Enter a noun in plural: ")
-number = input("Enter a number: ")
-number = input ("Enter another number: ")
+number1 = input("Enter a number: ")
+number2 = input ("Enter another number: ")
 shape = input("Enter a shape in plural: ")
 food1 = input("Enter a food: ")
 food2 = input("Enter a another food: ")
 
-print("Pizza was invented by a "+adj1+chef named . To make a piza, you need to take a lump of ")
+print("Pizza was invented by a " + adj1 + " " + nationality + " chef named " + name +
+      ". To make a pizza, you need to take a lump of " + noun + ", and make a thin round " + adj2 + " " +
+      noun2 + ". Then you cover it with " + adj3 + "sauce, " + adj4 + " cheese, and fresh chopped " + 
+      pluralnoun + ". Next you have to bake it in a very hot " + noun3 + ". When it is done, cut it into " +
+      number1 + " " + shape + ". Some people like " + food1 + " pizza the best, but my favorite is the " +
+      food2 + " pizza. If I could, I would eat pizza " + number2 + " times a day!")
